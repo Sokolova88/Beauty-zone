a6ec01f98d99e23616d302cb58ac1fdddb90ef47
Sokolova88 <[sokolova.studies@gmail.com]>
2024-02-01 01:27:14 +0200

    Add a mobile nav logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
 
 // import swiper from './modules/swiper.js';
 
