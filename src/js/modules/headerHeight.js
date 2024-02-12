e28b97aaed8f156fac3ebe2b32c96474883bd3e6
Sokolova88 <[sokolova.studies@gmail.com]>
2024-02-12 20:39:07 +0200

    Remove a console.log()

--- src/js/modules/headerHeight.js
+++ src/js/modules/headerHeight.js
@@ -1,7 +1,6 @@
 function headerHeight() {
   const { height: headerHeight } = document.querySelector('.header').getBoundingClientRect();
   document.body.style.paddingTop = `${headerHeight}px`;
-  console.log(document.body.style.paddingTop);
 }
 
 export default headerHeight;
